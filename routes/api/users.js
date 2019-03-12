5248c8effc6f32d1b190f0479a21c5d38f43fd8c
Nikhil Gupta <nikhilgupta311@gmail.com>
2019-03-12 11:41:30 +0530

    added: user login

--- routes/api/users.js
+++ routes/api/users.js
@@ -46,4 +46,31 @@ router.post('/register', (req, res) => {
     })
 });
 
+// @route   POST api/users/login
+// @desc    Login user / returning JWT token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find the user by email
+  User.findOne({email})
+    .then(user => {
+      // check for user
+      if(!user){
+        return res.status(404).json({email: 'User not found'});
+      }
+
+      // check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch){
+            res.json({msg: 'Success'});
+          } else{
+            return res.status(400).json({password: 'Password incorrect'});
+          }
+        });
+    });
+});
+
 module.exports = router;
