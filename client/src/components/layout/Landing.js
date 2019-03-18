8f9c82cd190d6fb830b1b3bd47794942768ff2da
Nikhil Gupta <nikhilgupta311@gmail.com>
2019-03-18 23:32:53 +0530

    added: basic Landing layout

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-4 mb-4 ">
+                  <strong>askDev</strong>
+                </h1>
+                <h3 className="display-5 mb-4">The Developer Connector</h3>
+                <p className="lead">
+                  {" "}
+                  Create a developer profile/portfolio, share posts and get help
+                  from other developers
+                </p>
+                <hr />
+                <a href="register.html" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </a>
+                <a href="login.html" className="btn btn-lg btn-light">
+                  Login
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
