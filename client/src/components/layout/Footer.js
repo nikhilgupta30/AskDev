b59a34baccd278ceb5820ce11700fd39f1e6c92f
Nikhil Gupta <nikhilgupta311@gmail.com>
2019-03-19 16:24:30 +0530

    removed: unwanted styling

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,11 +1,8 @@
-import React from "react";
+import React from 'react';
 
 export default function Footer() {
   return (
-    <footer
-      className="bg-dark text-white mt-5 p-3 text-center"
-      style={{ position: "fixed", bottom: 0, width: "100%" }}
-    >
+    <footer className="bg-dark text-white mt-5 p-3 text-center">
       Copyright &copy; {new Date().getFullYear()} askDev
     </footer>
   );
