6e2843158cbd83ae4524384257934684911ab617
Nikhil Gupta <nikhilgupta311@gmail.com>
2019-03-21 00:47:28 +0530

    added: login action of redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -12,4 +14,35 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login - Get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      // Save to local storage
+      const { token } = res.data;
+      localStorage.setItem('jwtToken', token);
+      // set token to Auth header
+      setAuthToken(token);
+      // decode token to get user data
+      const decoded = jwt_decode(token);
+      // set current user
+      dispatch(setCurrUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set Logged In User
+export const setCurrUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
