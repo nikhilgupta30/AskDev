522cfc2de6d48b8dc7c4affc2d30d383fb542f91
Nikhil Gupta <nikhilgupta311@gmail.com>
2019-03-21 00:53:38 +0530

    updated: now logged in user remains login after reloading page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrUser } from './actions/authActions';
+
 import { Provider } from 'react-redux'; // provides application with store which hold state
 import store from './store';
 
@@ -9,6 +13,16 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+
+// check for token
+if (localStorage.jwtToken) {
+  // set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // decode token and get user info and experation
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrUser(decoded));
+}
 
 class App extends Component {
   render() {
