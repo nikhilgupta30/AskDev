abbcef9d7b6d1ebd35ea32c079f0c03479cb6edb
Nikhil Gupta <nikhilgupta311@gmail.com>
2019-03-22 22:58:13 +0530

    modify: made dashboard route private

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrUser, logoutUser } from './actions/authActions';
@@ -8,6 +8,8 @@ import { clearCurrentProfile } from './actions/profileActions';
 
 import { Provider } from 'react-redux'; // provides application with store which hold state
 import store from './store';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -48,7 +50,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
             </div>
             <Footer />
           </div>
