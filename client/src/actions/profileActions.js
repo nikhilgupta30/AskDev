214bf964e0be0c727566a5f66ad077d3640451c7
Nikhil Gupta <nikhilgupta311@gmail.com>
2019-03-25 22:42:53 +0530

    added: delete account & profile action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -40,6 +40,26 @@ export const createProfile = (profileData, history) => dispatch => {
     );
 };
 
+// Delete account and profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm('Are you sure? This can NOT be undone!')) {
+    axios
+      .delete('/api/profile')
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
+};
+
 // profile loading
 export const setProfileLoading = () => {
   return {
