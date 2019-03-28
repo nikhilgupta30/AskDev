2747c77433f69c862d419be1cbd8b0d8565cee5a
Nikhil Gupta <nikhilgupta311@gmail.com>
2019-03-29 02:12:18 +0530

    added: complete ui for Profile page

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -16,6 +16,12 @@ class Profile extends Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.profile.profile === null && this.props.profile.loading) {
+      this.props.history.push('/not-found');
+    }
+  }
+
   render() {
     const { profile, loading } = this.props.profile;
 
@@ -33,10 +39,15 @@ class Profile extends Component {
               </Link>
             </div>
           </div>
-          <ProfileHeader />
-          <ProfileAbout />
-          <ProfileCreds />
-          <ProfileGithub />
+          <ProfileHeader profile={profile} />
+          <ProfileAbout profile={profile} />
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
+          {profile.githubusername ? (
+            <ProfileGithub username={profile.githubusername} />
+          ) : null}
         </div>
       );
     }
