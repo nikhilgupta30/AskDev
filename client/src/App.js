8b433b9041dd5f5b7c638bed427f60a3dfaa5c9b
Nikhil Gupta <nikhilgupta311@gmail.com>
2019-04-01 20:08:08 +0530

    added: post feed route to app

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,8 @@ import EditProfile from './components/edit-profile/EditProfile';
 import AddExpereince from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 
+import Posts from './components/posts/Posts';
+
 // check for token
 if (localStorage.jwtToken) {
   // set auth token header auth
@@ -90,6 +92,9 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
