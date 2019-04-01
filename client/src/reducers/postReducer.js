e1d494805ea3a3f6048d18adee5e4e3f6c3a013e
Nikhil Gupta <nikhilgupta311@gmail.com>
2019-04-01 12:58:29 +0530

    added: post reducer

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,21 @@
+import isEmpty from '../validation/isEmpty';
+import { ADD_POST } from '../actions/types';
+
+const initialState = {
+  posts: [],
+  post: {},
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        post: action.payload,
+        loading: false
+      };
+    default:
+      return state;
+  }
+}
